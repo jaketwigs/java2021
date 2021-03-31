1df7d8da329aa60b0a1824a6a2263fd898981bef
William Eipp <eippw@bpcsd.org>
2021-03-31 13:23:59 -0400

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,18 @@
+public class Matrix {
+    
+    // TODO: print out md one row per line
+    public void print(int[][] md) {
+ 
+    }
+    
+    // TODO: return the to sum of all rows
+    public int sumAllRows(int[][] md) {
+      return -1;
+    }
+    
+    // TODO: return the sum of a particular row given in rowIndex
+    //       make sure rowIndex is valid if not valid return -1
+    public int sumRow(int[][] md, int rowIndex) {
+      return -1;
+    }
+}
